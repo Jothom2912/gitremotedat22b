a6ea71e658350901b82dfef0638268d4520192e9
Jothom2912 <johannes1504@gmail.com>
2023-03-06 14:00:37 +0100

    blabla

---
+++ src/main/java/com/controller/Controller.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/Bla")
+            public String index()
+    {
+        return "hello world";
+    }
+
+    @GetMapping("/shout")
+    public String shout()
+    {
+        return "shout";
+    }
+
+    //bla bla bla
+}
